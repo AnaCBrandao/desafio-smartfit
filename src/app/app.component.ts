17baf7957acecedc4e4ad6dd5f6cf964be370819
anaBrandao <anacarolini633@gmail.com>
2024-03-30 20:06:23 -0300

    legend & footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import { RouterOutlet } from '@angular/router';
 
 import { HeaderComponent } from './components/header/header.component';
 import { FormComponent } from './components/form/form.component';
+import { LegendComponent } from './components/legend/legend.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FormComponent],
+  imports: [RouterOutlet, HeaderComponent, FormComponent, LegendComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
